5afbc717fa4e86927c4db4696bd31f4c2d8b561a
Moli@19931 <mallika454@gmail.com>
2020-05-11 19:25:48 +0400

    Last run on 11 may 2020

--- PlotyCovid19.py
+++ PlotyCovid19.py
@@ -16,17 +16,23 @@ df=df.dropna()
 #print(df[df.cases<0])
 df['cases']=df['cases'].abs()
 
-# Now back to your map plotting code...
+# It may also be interesting to plot cumulative cases over time:
+sdf = df.groupby(['countryCode','date']).sum().groupby('countryCode').cumsum().reset_index()
+sdf.rename(columns={'cases':'cum_cases'}, inplace=True)
+df = df.merge(sdf,on=['countryCode','date']) # Merge cumulative case data back into df
+df = df.sort_values(['date','countryCode']) # Re-sort
 fig = px.scatter_geo(
-    df,
-    locations='countryCode',
-    color='continent',
-    hover_name='country',
-    size='cases',
-    projection="natural earth",
-    title=f'World COVID-19 Cases',
-    animation_frame="date"
-    )
-#fig.show()
+df,
+locations='countryCode',
+color='continent',
+hover_name='country',
+size="cum_cases",
+projection="natural earth",
+title=f'World COVID-19 Cumulative Cases',
+animation_frame="date"
+)
 
-pio.write_html(fig, file="index.html", auto_open=True)
+#fig2 = px.bar(df, x="cum_cases", y="country", orientation='h', animation_frame="date")
+#fig2.show()
+
+pio.write_html((fig), file="index.html", auto_open=True)
